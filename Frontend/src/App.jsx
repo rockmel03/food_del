6a01d135ab11a66b658524a3e5c880191097cd9e
rockmel03 <kamalswag2@gmail.com>
2024-05-10 00:18:33 +0530

    first commit -project setup done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./components/index";
+
+const App = () => {
+  return (
+    <main className="min-h-screen px-[3vw]">
+      <Navbar />
+    </main>
+  );
+};
+
+export default App;
