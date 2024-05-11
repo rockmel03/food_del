95713591cfd36f97ec14e05063a055bed1081881
rockmel03 <kamalswag2@gmail.com>
2024-05-11 23:35:54 +0530

    added store context

---
+++ Frontend/src/context/StoreContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useReducer } from "react";
+import { food_list } from "../assets";
+
+export const StoreContext = createContext({});
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "add-to-cart": {
+      if (!state[action.payload]) {
+        return { ...state, [action.payload]: 1 };
+      }
+      return { ...state, [action.payload]: state[action.payload] + 1 };
+    }
+    case "remove-from-cart": {
+      return { ...state, [action.payload]: state[action.payload] - 1 };
+    }
+  }
+};
+
+
+function StoreContextProvider({ children }) {
+  const [cartItems, dispatchCart] = useReducer(cartReducer, {});
+
+  useEffect(() => {
+    console.log(cartItems)
+  },[cartItems])
+
+  let contextValue = {
+    food_list,
+    cartItems,
+    dispatchCart,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {children}
+    </StoreContext.Provider>
+  );
+}
+
+export default StoreContextProvider;
