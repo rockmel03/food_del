ad48dead763349d013d118ab9d6c0e2e5315b327
rockmel03 <kamalswag2@gmail.com>
2024-05-13 00:30:51 +0530

    added input field component

---
+++ Frontend/src/components/templets/InputField.jsx
@@ -0,0 +1,22 @@
+import React, { useId } from "react";
+
+const InputField = React.forwardRef(function InputField(
+  { label, type, className, ...props },
+  ref
+) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        ref={ref}
+        id={id}
+        type={type || "text"}
+        className={`px-4 py-2 bg-transparent text-inherit rounded border-none ${className} w-full`}
+        {...props}
+      />
+    </div>
+  );
+});
+
+export default InputField;
