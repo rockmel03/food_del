e4b801c6b797b8e6b9dc47133ff4e5a7dfba7a7e
rockmel03 <kamalswag2@gmail.com>
2024-05-15 23:52:42 +0530

    B- added food model

---
+++ Backend/src/models/food.model.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const foodSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, default: 0 },
+    description: { type: String, required: true },
+    category: { type: String, required: true }
+})
+
+
+export default mongoose.model('Food', foodSchema)
