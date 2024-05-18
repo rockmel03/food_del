728423fd6999444948fe1d91caacf1c0fce8faea
rockmel03 <kamalswag2@gmail.com>
2024-05-18 19:32:37 +0530

    A - ui changes

--- Admin/src/Pages/FoodList.jsx
+++ Admin/src/Pages/FoodList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Axios from "../utils/axios";
+import { Link } from "react-router-dom";
 
 export const FoodList = () => {
   const [FoodList, setFoodList] = useState([]);
@@ -21,44 +22,55 @@ export const FoodList = () => {
   };
 
   useEffect(() => {
-    fetchData();  
+    fetchData();
   }, []);
 
   return (
-    <section className="w-full flex flex-col items-center py-10 px-5">
-      <section className="w-full">
-        <h2>ALL Food List</h2>
-        <div className="w-full hidden sm:grid grid-cols-[0.5fr__1.5fr_0.75fr_1fr_0.75fr] gap-2 py-2">
-          <b>Item</b>
-          <b>Title</b>
-          <b>price</b>
-          <b>category</b>
-          <b>actions</b>
+    <main className="w-full py-5">
+      <h1 className="text-center text-xl font-bold">Food List</h1>
+
+      <section className="p-5">
+        <hr className="hidden sm:block" />
+        <br className="hidden sm:block" />
+        <div className="hidden w-full sm:grid grid-cols-[1fr_1.5fr_0.5fr_1fr_1fr] gap-2 items-center">
+          <b>Image</b>
+          <b>Item Name</b>
+          <b>Price</b>
+          <b>Category</b>
+          <b>Actions</b>
         </div>
+        <br />
         <hr />
-        {FoodList.map((item, index) => (
-          <div key={item._id}>
-            <article className="w-full grid grid-cols-[1fr_2fr_.5fr] sm:grid-cols-[0.5fr__1.5fr_0.75fr_1fr_0.75fr] gap-2 items-center grid-wrap py-5">
-              <div className="w-full aspect-video ">
-                <img
-                  src={
-                    import.meta.env.VITE_SERVER_URL + `/images/${item.image}`
-                  }
-                  alt=""
-                  className="w-full h-full object-contain"
-                />
+
+        {FoodList.map((food) => (
+          <article key={food._id}>
+            <div className="w-full grid grid-cols-[1fr_2fr_1fr] sm:grid-cols-[1fr_1.5fr_0.5fr_1fr_1fr] gap-x-3 gap-y-4 items-center py-2">
+              <Link to={`/details/${food._id}`}>
+                <div className="max-w-[20vw] sm:max-w-[12vh]">
+                  <img
+                    src={`${import.meta.env.VITE_SERVER_URL}/images/${
+                      food.image
+                    }`}
+                    alt={food.name}
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+              </Link>
+              <Link to={`/details/${food._id}`}>
+                <p className="capitalize hover:underline hover:text-blue-500">
+                  {food.name}
+                </p>
+              </Link>
+              <p className="">${food.price}</p>
+              <p className="">{food.category}</p>
+              <div className="flex gap-2">
+                <button onClick={()=> deleteHandler(food._id)} className="px-2 text-red-500">Delete</button>
               </div>
-              <p>{item.name}</p>
-              <p>${item.price}</p>
-              <p>{item.category}</p>
-              <p className="cursor-pointer" onClick={() => deleteHandler(item._id)}>
-                delete
-              </p>
-            </article>
+            </div>
             <hr />
-          </div>
+          </article>
         ))}
       </section>
-    </section>
+    </main>
   );
 };
