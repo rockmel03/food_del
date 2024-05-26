ffcac503de25e3696848af0daf8d87d7b7378728
rockmel03 <kamalswag2@gmail.com>
2024-05-26 23:54:54 +0530

    F- update cart data on database and fixed issues related to cors

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -3,21 +3,74 @@ import Axios from "../utils/Axios";
 
 export const StoreContext = createContext({});
 
+const fetchFoodList = async () => {
+  try {
+    const response = await Axios.get("/api/v1/food/list");
+    if (response.data.status) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.log("Error: failed to get food list", error);
+    throw new Error("Failed to get food list");
+  }
+};
+
+const fetchCartList = async () => {
+  try {
+    const response = await Axios.get("/api/v1/cart");
+    if (response.data.status) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.log("Error: failed to fetch cart data", error);
+    throw new Error("Failed to fetch cart data");
+  }
+};
+
+const addToCart = async (itemId) => {
+  try {
+    const response = await Axios.post(`/api/v1/cart/${itemId}`);
+    if (response.data.status) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error while adding to cart");
+  }
+};
+
+const removeFromCart = async (itemId) => {
+  try {
+    const response = await Axios.patch(`/api/v1/cart/${itemId}`);
+    if (response.data.status) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error while adding to cart");
+  }
+};
+
 const cartReducer = (state, action) => {
   switch (action.type) {
     case "add-to-cart": {
+      addToCart(action.payload);
       if (!state[action.payload]) {
         return { ...state, [action.payload]: 1 };
       }
       return { ...state, [action.payload]: state[action.payload] + 1 };
     }
     case "remove-from-cart": {
+      removeFromCart(action.payload);
       if (state[action.payload] === 1) {
         const newState = { ...state };
         delete newState[action.payload];
         return newState;
       }
       return { ...state, [action.payload]: state[action.payload] - 1 };
+    }
+    case "set-cart": {
+      return action.payload;
     }
   }
 };
@@ -29,21 +82,26 @@ function StoreContextProvider({ children }) {
 
   const [cartItems, dispatchCart] = useReducer(cartReducer, {});
 
-  const fetchFoodList = async () => {
-    const response = await Axios.get("/api/v1/food/list");
-    const { data } = response;
-    return data.data;
-  };
-
   useEffect(() => {
     // fetch all food
-    fetchFoodList().then((data) => setFood_list(data));
+    fetchFoodList()
+      .then((data) => setFood_list(data))
+      .catch((err) => alert(err.message));
 
     // set the access token
     if (localStorage.getItem("access_token")) {
       setAccessToken(localStorage.getItem("access_token"));
     }
   }, []);
+
+  useEffect(() => {
+    if (accessToken) {
+      // set cart items
+      fetchCartList()
+        .then((data) => dispatchCart({ type: "set-cart", payload: data }))
+        .catch((error) => alert(error.message));
+    }
+  }, [accessToken]);
 
   const getTotalCartAmount = () => {
     return Object.keys(cartItems)
--- Frontend/src/utils/Axios.js
+++ Frontend/src/utils/Axios.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const Axios = axios.create({
     baseURL: import.meta.env.VITE_SERVER_URL,
+    withCredentials: true,  // Include credentials such as cookies in the request
 })
 
 
