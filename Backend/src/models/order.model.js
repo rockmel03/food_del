ea9c7f4403638716ef4cb951e17df524c6ba769f
rockmel03 <kamalswag2@gmail.com>
2024-05-29 00:15:20 +0530

    B - changes made on order model

--- Backend/src/models/order.model.js
+++ Backend/src/models/order.model.js
@@ -2,17 +2,25 @@ import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    address: { type: Object, required: true },
+    address: {
+        email: { type: String, required: true },
+        phone: { type: String, required: true },
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        country: { type: String, required: true },
+        zipcode: { type: Number, required: true },
+    },
     foods: [
         {
-            item: { type: mongoose.Schema.Types.ObjectId, ref: 'Food', required: true },
+            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Food', required: true },
             quantity: { type: Number, default: 1, min: 1 },
         }
     ],
     total: { type: Number, default: 0, min: 0 },
     status: { type: String, default: 'pending', enum: ['pending', 'confirmed', 'shipped', 'delivered', 'cancelled'] },
-    paymentStatus: { type: Boolean, default: false },
-})
+    payment: { type: Boolean, default: false },
+}, { timestamps: true })
 
 const Order = mongoose.model('Order', orderSchema);
 
